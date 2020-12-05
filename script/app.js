f1db8274595a2df79bb29c0caeba2c580c4adfe7
Ayko Geeraert <ayko.geeraert@student.howest.be>
2020-12-05 11:11:28 +0100

    Labo

--- script/app.js
+++ script/app.js
@@ -1,95 +1,62 @@
-let daySelect, graph;
+let timeOut;
 
-const drawChart = (data) => {
-	let ctx = graph.getContext('2d');
-
-	new Chart(ctx, {
-		type: 'line',
-		data: {
-			labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6', 'Week 7'],
-			datasets: [
-				{
-					label: 'Visitors',
-					data: data,
-					borderColor: '#A3A0FB',
-					backgroundColor: '#A3A0FB10',
-					pointBackgroundColor: 'white',
-					lineTension: 0.3,
-					borderWidth: 2,
-					pointRadius: 4,
-				},
-			],
-		},
-		options: {
-			defaultFontColor: (Chart.defaults.global.defaultFontColor = '#808495'),
-			scales: {
-				yAxes: [
-					{
-						ticks: {
-							min: 0,
-							max: 50,
-						},
-					},
-				],
-			},
-			tooltips: {
-				xPadding: 10,
-				yPadding: 10,
-				cornerRadius: 0,
-			},
-			legend: {
-				position: 'bottom',
-				align: 'start',
-				labels: {
-					defaultFontFamily: (Chart.defaults.global.defaultFontFamily = "'Source Sans Pro', 'Helvetica', 'arial', 'sans-serif'"),
-					boxWidth: 2,
-				},
-			},
-			responsive: true,
-		},
-	});
-};
-
-const getData = (json) => {
-	let data = [];
-
-	json.map((day) => {
-		data.push(day.aantalBezoekers);
+function handleDropdownButton() {
+	let show = 'is-open',
+		elements = document.getElementsByClassName('js-dropdown-toggle');
+	for (let i = 0; i < elements.length; i++) {
+		elements[i].addEventListener('click', function() {
+			removeClass(elements, show);
+			if (hasClass(this, show)) {
+				removeClass(this, show);
+			} else {
+				addClass(this, show);
+			}
+		});
+	}
+	document.addEventListener('click', function(event) {
+		for (let i = 0; i < elements.length; i++) {
+			if (event.target.closest('.js-dropdown-toggle')) return;
+			removeClass(elements[i], show);
+		}
 	});
 
-	drawChart(data);
-	//hideLoader();
-};
+	function addClass(elements, myClass) {
+		if (!elements) {
+			return;
+		}
+		if (typeof elements === 'string') {
+			elements = document.querySelectorAll(elements);
+		} else if (elements.tagName) {
+			elements = [elements];
+		}
+		for (let i = 0; i < elements.length; i++) {
+			if ((' ' + elements[i].className + ' ').indexOf(' ' + myClass + ' ') < 0) {
+				elements[i].className += ' ' + myClass;
+			}
+		}
+	}
+	
+	function removeClass(elements, myClass) {
+		if (!elements) {
+			return;
+		}
+		if (typeof elements === 'string') {
+			elements = document.querySelectorAll(elements);
+		} else if (elements.tagName) {
+			elements = [elements];
+		}
+		let reg = new RegExp('(^| )' + myClass + '($| )', 'g');
+		for (let i = 0; i < elements.length; i++) {
+			elements[i].className = elements[i].className.replace(reg, ' ');
+		}
+	}
 
-const getVisitorsByDay = (day) => {
-	//showLoader();
+	function hasClass(element, myClass) {
+		return (' ' + element.className + ' ').indexOf(' ' + myClass + ' ') > -1;
+	}
+}
 
-	const endpoint = `https://iotcloud-mct.azurewebsites.net/api/visitors/${day}`;
-
-	fetch(endpoint)
-		.then((r) => r.json())
-		.then((json) => {
-			getData(json);
-		})
-		.catch((e) => console.error(e));
-};
-
-const init = () => {
-	daySelect = document.querySelector('.js-day-select');
-	graph = document.querySelector('.js-graph');
-
-	daySelect.addEventListener('change', function (e) {
-		getVisitorsByDay(e.target.value);
-	});
-
-	canvas = document.querySelector('.js-graph');
-	loaderContainer = document.querySelector('.js-load-container');
-	loader = document.querySelector('.js-loader');
-
-	getVisitorsByDay('maandag');
-};
-
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Script geladen!");
-    init();
+document.addEventListener('DOMContentLoaded', function() {
+	console.log('Script loaded!');
+	handleDropdownButton();
 });
---
+++ script/dashboard-chart.js
@@ -0,0 +1,111 @@
+let daySelect, graph, canvas, loaderContainer, loaderDelay, loader;
+
+const hideLoader = function () {
+	clearTimeout(loaderDelay);
+
+	loaderContainer.style.display = 'none';
+	canvas.style.display = 'block';
+	loader.style.opacity = 0;
+};
+
+const showLoader = function () {
+	loaderContainer.style.display = 'flex';
+	canvas.style.display = 'none';
+
+	loaderDelay = setTimeout(() => {
+		loader.style.opacity = 1;
+	}, 1000);
+};
+
+const drawChart = (data) => {
+	let ctx = graph.getContext('2d');
+
+	new Chart(ctx, {
+		type: 'line',
+		data: {
+			labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6', 'Week 7'],
+			datasets: [
+				{
+					label: 'Visitors',
+					data: data,
+					borderColor: '#A3A0FB',
+					backgroundColor: '#A3A0FB10',
+					pointBackgroundColor: 'white',
+					lineTension: 0.3,
+					borderWidth: 2,
+					pointRadius: 4,
+				},
+			],
+		},
+		options: {
+			defaultFontColor: (Chart.defaults.global.defaultFontColor = '#808495'),
+			scales: {
+				yAxes: [
+					{
+						ticks: {
+							min: 0,
+							max: 50,
+						},
+					},
+				],
+			},
+			tooltips: {
+				xPadding: 10,
+				yPadding: 10,
+				cornerRadius: 0,
+			},
+			legend: {
+				position: 'bottom',
+				align: 'start',
+				labels: {
+					defaultFontFamily: (Chart.defaults.global.defaultFontFamily = "'Source Sans Pro', 'Helvetica', 'arial', 'sans-serif'"),
+					boxWidth: 2,
+				},
+			},
+			responsive: true,
+		},
+	});
+};
+
+const getData = (json) => {
+	let data = [];
+
+	json.map((day) => {
+		data.push(day.aantalBezoekers);
+	});
+
+	drawChart(data);
+	hideLoader();
+};
+
+const getVisitorsByDay = (day) => {
+	showLoader();
+
+	const endpoint = `https://iotcloud-mct.azurewebsites.net/api/visitors/${day}`;
+
+	fetch(endpoint)
+		.then((r) => r.json())
+		.then((json) => {
+			getData(json);
+		})
+		.catch((e) => console.error(e));
+};
+
+const init = () => {
+	daySelect = document.querySelector('.js-day-select');
+	graph = document.querySelector('.js-graph');
+
+	daySelect.addEventListener('change', function (e) {
+		getVisitorsByDay(e.target.value);
+	});
+
+	canvas = document.querySelector('.js-graph');
+	loaderContainer = document.querySelector('.js-load-container');
+	loader = document.querySelector('.js-loader');
+
+	getVisitorsByDay('maandag');
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+	init();
+});
